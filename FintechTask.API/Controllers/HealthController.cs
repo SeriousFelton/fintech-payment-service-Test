9c8724168f9458b7e28af4f1527062d2fa70cdfb
SeriousFelton <SeriousFelton@gmail.com>
2026-07-29 20:52:10 +0700

    Создание HealthController
    - Добавление GET /health эндпоинта
    - Возврат json со статусом "healthy" и времени

---
+++ FintechTask.API/Controllers/HealthController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FintechTask.API.Controllers
+{
+    [ApiController]
+    [Route("health")]
+    public class HealthController : ControllerBase
+    {
+        private readonly ILogger<HealthController> _logger;
+        
+        public HealthController(ILogger<HealthController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Проверка контроллера Health");
+            return Ok(new { status = "healthy", timestamp = DateTime.UtcNow });
+        }
+    }
+}
